f23db9c781bbda0e485cb03caf58dfa40d6e4423
ronycse16b <rony.cse16b@gmail.com>
2022-11-08 14:35:20 +0600

    naver added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navber from './pages/Shared/Navber/Navber';
+import Footer from './pages/Shared/Footer/Footer';
+import Topber from './pages/Shared/Topber/Topber';
+import Banner from './pages/Banner/Banner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="  mx-auto">
+      <Topber></Topber>
+      <Navber></Navber>
+      <Banner></Banner>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/pages/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import bg from './imges/bg.jpg'
+
+const Banner = () => {
+    return (
+        <div className='min-h-screen' style={{ backgroundImage:`url(${bg})` }}>
+            <div className=''>
+           
+<h2 >Powerful Digital <strong>IT solutions Company</strong></h2>
+<p >
+Affixed pretend account ten natural. Need eat week even yet that. Incommode delighted he resolving sportsmen do in listening.
+</p><a >Find out More</a></div>
+            
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
